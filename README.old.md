72215fc0bfb87016de2515d8c2dbd2291e3a172c
Ethan Lee <ethethanlee@gmail.com>
2023-09-28 11:31:51 -0700

    react app

---
+++ README.old.md
@@ -0,0 +1,5 @@
+# p-geolocation
+Web app to estimate a user's location with two images of the sun. 
+
+# To create the React App:
+> npx create-react-app p-geolocation
---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import MapComponent from './MapComponent';
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hi! Welcome to p-geolocation. Here is a map component: </h1> <br />
+        {/* <MapComponent /> */}
+      </div>
+    );
+  }
+}
+
+export default App;

5ad897948356e92f78ac6a8a2d8d25dc77489176
Ethan Lee <ethethanlee@gmail.com>
2023-11-01 19:35:20 -0700

    i dont have xcode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import MapComponent from './mapcomponent';
 import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <View style={styles.container}>
         <h1>Hi! Welcome to p-geolocation. Here is a map component: </h1> <br />
         <MapComponent />
-      </div>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;

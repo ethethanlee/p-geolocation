e6ffc91aafa63cb3014e7f8d1dd494ceb57da490
aimeekco <aimeekco@gmail.com>
2023-11-05 14:47:47 -0800

    Co-authored-by: George Axarlis <georgeaxa@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hi! Welcome to p-geolocation. Here is a map component: </h1> <br />
+        <h1>Hi! Welcome to p-geolocation. </h1> <br />
         <MapComponent />
       </div>
     );

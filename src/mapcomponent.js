b9b2b25f915d3e7404cc9073617cf6df4d7751e4
aimeekco <aimeekco@gmail.com>
2023-11-14 16:38:14 -0800

    hi

--- src/mapcomponent.js
+++ src/mapcomponent.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import { Loader } from '@googlemaps/js-api-loader';
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> be240e9 (hi)
 /**
  * Initializes and loads a Google map with a marker on a specified location embedded in the webpage
  */
 export class MapComponent extends Component {
+<<<<<<< HEAD
   constructor(props) {
     super(props);
     this.map = null;
@@ -48,13 +52,15 @@ export class MapComponent extends Component {
   
 =======
 class MapComponent extends Component {
+=======
+>>>>>>> be240e9 (hi)
   constructor(props) {
     super(props);
     this.map = null;
     this.loader = new Loader({  
       apiKey: process.env.REACT_APP_API_KEY, 
-      version: "weekly",
-      libraries: ["places"],
+      version: 'weekly',
+      libraries: ['places'],
     });
   }
 
@@ -71,10 +77,10 @@ class MapComponent extends Component {
       const position = { lat: 34.100010, lng: -117.714802 };
 
       // Create a map
-      this.map = new window.google.maps.Map(document.getElementById("map"), {
+      this.map = new window.google.maps.Map(document.getElementById('map'), {
         zoom: 17,
         center: position,
-        mapId: "estella",
+        mapId: 'estella',
       });
 
       console.log('Map initialized');
@@ -83,7 +89,7 @@ class MapComponent extends Component {
       new window.google.maps.Marker({
         position: position,
         map: this.map,
-        title: "Estella",
+        title: 'Estella',
       });
     });
   }
@@ -98,8 +104,12 @@ class MapComponent extends Component {
         {/* <MapComponent /> */}
 =======
         <h1>Here is your location:</h1>
+<<<<<<< HEAD
         <div id="map" style={{ width: '100%', height: '550px' }}></div>
 >>>>>>> e72793c (new new map component)
+=======
+        <div id='map' style={{ width: '100%', height: '550px' }}></div>
+>>>>>>> be240e9 (hi)
       </div>
     );
   }
@@ -109,8 +119,11 @@ class MapComponent extends Component {
 <<<<<<< HEAD
 =======
 
+<<<<<<< HEAD
 >>>>>>> faa2d2d (map w api key)
 export default MapComponent;
 =======
 export default MapComponent;
 >>>>>>> e72793c (new new map component)
+=======
+>>>>>>> be240e9 (hi)

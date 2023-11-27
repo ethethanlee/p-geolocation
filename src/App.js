aa88e3141f07165e7ec82d47979703ddf5391723
Ethan Lee <ethethanlee@gmail.com>
2023-11-27 12:21:29 -0800

    added some stuff

--- src/App.js
+++ src/App.js
@@ -4,22 +4,66 @@ import React, { Component } from 'react';
 class App extends Component {
   // add a button to call function fetch. 
   // <button onclick=function>
+
+  click = () => {
+    // need to figure out how to upload an image to flask TODO (2 IMAGES)
+    // make suer in flask to methods=['POST']
+    console.log('HGa;slkdjfa;osdifjkl')
+
+    // right now, the button calls this click function which just 
+    // prints to the console which u can find from inspect element -> console
+  }
+  
+// TODO
+  // below is some chatgpt code to start from:
+
+  // const [selectedFile, setSelectedFile] = useState(null);
+
+  // handleFileChange = (event) => {
+  //   setSelectedFile(event.target.files[0]);
+  // };
+
+  // handleUpload = () => {
+  //   const formData = new FormData();
+  //   formData.append('file', selectedFile);
+
+  //   fetch('http://your-flask-server/upload', {
+  //     method: 'POST',
+  //     body: formData
+  //   })
+  //   .then(response => {
+  //     if (!response.ok) {
+  //       throw new Error('Network response was not ok');
+  //     }
+  //     return response.json();
+  //   })
+  //   .then(data => {
+  //     console.log(data);
+  //     // Handle success or update UI as needed
+  //   })
+  //   .catch(error => {
+  //     console.error('Error uploading the image:', error);
+  //   });
+  // };
   
   render() {
 
-    fetch('/sun_data_route')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      // Handle the data in your React app
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-
+    // This will go in a function that is called when the button below is pressed!
+    // fetch('/sun_data_route')
+    // .then(response => response.json())
+    // .then(data => {
+    //   console.log(data);
+    //   // Handle the data in your React app
+    // })
+    // .catch(error => {
+    //   console.error('Error:', error);
+    // });
+    // const 
     return (
       <div>
         <h1>Hi! Welcome to p-geolocation. </h1> <br />
+        <button onClick={() => this.click()}>Click this!</button>
+
         <MapComponent />
       </div>
     );

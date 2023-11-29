674b099579b47166ec6e450a1eb6558f904840c4
Ethan Lee <ethethanlee@gmail.com>
2023-11-28 23:28:43 -0800

    ok this has caleb's commits i think

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,23 @@
 from flask import Flask, request, jsonify
 from sun import process_data
+# from flask_cors import CORS
 
 app = Flask(__name__)
+# CORS(app)
 
-@app.route('/sun_data_route', methods=['GET'])
+
+@app.route('/sun_data_route/<locationInput>', methods=['GET'])
 def get_data(image_path):
-    # Call the process_data function with the provided image_path
     data = process_data(image_path)
     # Data isn't going to be date, but longitute data.
-    print("json: " , jsonify(data))
+
+    # we have to put data through what current.py does to get longitude and latitude
+
+    # we have to update process_data so that it uses the pixel coordinates we get from sun_locater 
+    # goes through the functions in current.py
+    # return('hi')
     return jsonify(data)
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
-
-get_data("src/sunlocater/sunimage1.jpeg")
+    app.run(debug=True)
--- frontend/App.js
+++ frontend/App.js
@@ -2,67 +2,59 @@ import {MapComponent} from './mapcomponent';
 import React, { Component } from 'react';
 
 class App extends Component {
-  // add a button to call function fetch. 
-  // <button onclick=function>
-
-  click = () => {
-    // need to figure out how to upload an image to flask TODO (2 IMAGES)
-    // make suer in flask to methods=['POST']
-    console.log('HGa;slkdjfa;osdifjkl')
-
-    // right now, the button calls this click function which just 
-    // prints to the console which u can find from inspect element -> console
+  constructor(props) {
+    super(props);
+    this.state = {
+      coordinates: null,
+      locationInput: '', // assuming you have this state variable
+    };
   }
-  
-// TODO
-  // below is some chatgpt code to start from:
 
-  // const [selectedFile, setSelectedFile] = useState(null);
+  handleInputChange = (event) => {
+    this.setState({ locationInput: event.target.value });
+  };
 
-  // handleFileChange = (event) => {
-  //   setSelectedFile(event.target.files[0]);
-  // };
+  fetchFunction = () => {
+    const { locationInput } = this.state;
 
-  // handleUpload = () => {
-  //   const formData = new FormData();
-  //   formData.append('file', selectedFile);
+    // Implement your fetch logic here
+    fetch(`http://127.0.0.1:3000/sun_data_route/${encodeURIComponent(locationInput)}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // Handle the data in your React app
+        this.setState({ coordinates : data });
+      // Handle the data in your React app
+      })
+      .catch(error => {
+        console.error('Error!!!:', error);
+      });
+        console.log('Fetching data...');
+  };
 
-  //   fetch('http://your-flask-server/upload', {
-  //     method: 'POST',
-  //     body: formData
-  //   })
-  //   .then(response => {
-  //     if (!response.ok) {
-  //       throw new Error('Network response was not ok');
-  //     }
-  //     return response.json();
-  //   })
-  //   .then(data => {
-  //     console.log(data);
-  //     // Handle success or update UI as needed
-  //   })
-  //   .catch(error => {
-  //     console.error('Error uploading the image:', error);
-  //   });
-  // };
-  
   render() {
+    // setting up variable
+    const { coordinates, locationInput } = this.state;
 
-    // This will go in a function that is called when the button below is pressed!
-    // fetch('/sun_data_route')
-    // .then(response => response.json())
-    // .then(data => {
-    //   console.log(data);
-    //   // Handle the data in your React app
-    // })
-    // .catch(error => {
-    //   console.error('Error:', error);
-    // });
-    // const 
     return (
       <div>
-        <h1>Hi! Welcome to p-geolocation. </h1> <br />
-        <button onClick={() => this.click()}>Click this!</button>
+        <h1>Hi! Welcome to p-geolocation. Here is a map component: </h1> <br />
+
+        <label>
+          Enter Image Path:
+          <input type="text" value={locationInput} onChange={this.handleInputChange} />
+        </label>
+
+        {/* Button to trigger the fetch function */}
+        <button onClick={this.fetchFunction}>Fetch Data!</button>
+
+        {/* Display Coordinates if they exist*/}
+        {coordinates && (
+          <div>
+            <p>Latitude: {coordinates.longitude}</p>
+            <p>Longitude: {coordinates.latitude}</p>
+          </div>
+        )}
 
         <MapComponent />
       </div>
